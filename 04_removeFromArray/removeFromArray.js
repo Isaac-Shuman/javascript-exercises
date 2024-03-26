c3758d62252ef7a6b48f31e8cf506e6abc9b044e
Isaac-Shuman@github.com <isaacshuman5@gmail.com>
2024-03-26 12:06:04 -0700

    finished 1-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    res = array;
+    for (const arg of args)
+    {   
+        var index = res.indexOf(arg);
+        while (index != -1) {
+        res.splice(index, 1);
+        index = res.indexOf(arg);
+        }
+    }
+    return res;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(low, high) {
+    if (!(typeof(low) === 'number' && typeof(high) === 'number') || low < 0 || high < 0)
+        return 'ERROR';
+    var sum = 0;
+    for (var i = Math.min(low, high); i <= Math.max(low, high); i++)
+        sum += i;
+    return sum;
 };
 
 // Do not edit below this line
